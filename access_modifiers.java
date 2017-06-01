78d186e2c3bbbf3f7ad5c1ceb5d724452cde9cec
Smita Kadam <smita.kadam@technicolor.com>
2017-06-01 12:12:14 +0530

    new commit

---
+++ access_modifiers.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class access_modifiers {
+	
+	public String name="smita";
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void testAccessModifier(){
+		System.out.println("public access method");
+	}
+
+}
